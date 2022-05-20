bac805ff6f3566a08300060fbc2972dc2a478de7
Johnny <JohnnyLieu@yahoo.com>
2022-05-20 15:01:10 -0700

    able to grab all contacts from phonebook cloud

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,3 +1,18 @@
-export const getContacts = (req, res) => {
-    res.send('router is working');
+import contact from '../models/contact.js';
+
+export const getContacts = async (req, res) => {
+    try {
+        const allContacts = await contact.find();
+        res.status(200).json(allContacts);
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+export const createContact = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error.message);   
+    }
 };
--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getContacts } from '../controllers/contact.js';
+import { getContacts, createContact } from '../controllers/contact.js';
+import contact from '../models/contact.js';
 
 const router = express.Router();
 
 router.get('/', getContacts);
+router.post('/', createContact);
 
 export default router;
