ee6b37fc0479a6d661e2e45c2cda7376e2763a70
Johnny <JohnnyLieu@yahoo.com>
2022-06-12 15:40:50 -0700

    testing

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import * as variables from './noneyo.js';
 import contactRoutes from './routes/contact.js';
+// const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +14,10 @@ app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 
 app.use(cors());
 app.use('/contacts', contactRoutes);
+
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static(path.join(__dirname, '../client/build')))
+// }
 
 mongoose.connect((process.env.MONGODB_URI || variables.REACT_APP_URL), {
     useNewUrlParser: true, useUnifiedTopology:true
