7d3a42c8a59140cc5aea7e6d015d6d53d4485fc5
Johnny <JohnnyLieu@yahoo.com>
2022-06-12 15:01:44 -0700

    edited port

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import * as variables from './noneyo.js';
 import contactRoutes from './routes/contact.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
@@ -15,6 +16,6 @@ app.use('/contacts', contactRoutes);
 
 mongoose.connect((variables.REACT_APP_URL), {
     useNewUrlParser: true, useUnifiedTopology:true
-}).then(() => app.listen(variables.PORT, () => console.log(`Listening on port: ${variables.PORT}`))).catch((err)=> console.log(err.message));
+}).then(() => app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))).catch((err)=> console.log(err.message));
 
 // mongoose.set('useFindAndMofidy', false); // this is crashing the server for some reason
